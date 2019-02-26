0197cdf416ce5fcaa067a966e458c128c7f8fbc6
ZhangxingBian <47854957+ZhangxingBian@users.noreply.github.com>
2019-02-26 16:43:03 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,104 @@
+import random
+import numpy as np
+import cv2
+
+def get_square(img, pos):
+    """Extract a left or a right square from ndarray shape : (H, W, C))"""
+    h = img.shape[0]
+    if pos == 0:
+        return img[:, :h]
+    else:
+        return img[:, -h:]
+
+def split_img_into_squares(img):
+    return get_square(img, 0), get_square(img, 1)
+
+def hwc_to_chw(img):
+    return np.transpose(img, axes=[2, 0, 1])
+
+def resize_and_crop(pilimg, scale=0.5, final_height=None):
+    w = pilimg.size[0]
+    h = pilimg.size[1]
+    newW = int(w * scale)
+    newH = int(h * scale)
+
+    if not final_height:
+        diff = 0
+    else:
+        diff = newH - final_height
+
+    img = pilimg.resize((newW, newH))
+    img = img.crop((0, diff // 2, newW, newH - diff // 2))
+    return np.array(img, dtype=np.float32)
+
+def resize2(img, SizeX,SizeY):
+    # for 3 channel
+    img=img.transpose((1,2,0))
+    img=cv2.resize(img,(SizeY,SizeX))
+    img = img.transpose((2,0,1))
+    return img
+
+def resize3(img, SizeX,SizeY):
+    # for 1 channel
+    img=cv2.resize(img,(SizeY,SizeX))
+    return img
+
+def cvt2binary(img):
+    Bi=img==255
+    Bi=Bi*1
+    return Bi
+
+def batch(iterable, batch_size):
+    """Yields lists by batch"""
+    b = []
+    for i, t in enumerate(iterable):
+        b.append(t)
+        if (i + 1) % batch_size == 0:
+            yield b
+            b = []
+
+    if len(b) > 0:
+        yield b
+
+def split_train_val(dataset, val_percent=0.05):
+    dataset = list(dataset)
+    length = len(dataset)
+    n = int(length * val_percent)
+    random.shuffle(dataset)
+    return {'train': dataset[:-n], 'val': dataset[-n:]}
+
+
+def normalize(x):
+    return x / 255
+
+def merge_masks(img1, img2, full_w):
+    h = img1.shape[0]
+
+    new = np.zeros((h, full_w), np.float32)
+    new[:, :full_w // 2 + 1] = img1[:, :full_w // 2 + 1]
+    new[:, full_w // 2 + 1:] = img2[:, -(full_w // 2 -1):]
+
+    return new
+
+def calIOU(mask, gt):
+    eps = 0.0001
+    union=mask|gt
+    mask=mask*1
+    gt=gt*1
+    inter = np.dot(mask.ravel(), gt.ravel())
+    # union = np.sum(mask) + np.sum(gt) + eps
+    t = (1. * inter + eps) / np.sum(union)
+    return t
+
+# credits to https://stackoverflow.com/users/6076729/manuel-lagunas
+def rle_encode(mask_image):
+    pixels = mask_image.flatten()
+    # We avoid issues with '1' at the start or end (at the corners of
+    # the original image) by setting those pixels to '0' explicitly.
+    # We do not expect these to be non-zero for an accurate mask,
+    # so this should not harm the score.
+    pixels[0] = 0
+    pixels[-1] = 0
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 2
+    runs[1::2] = runs[1::2] - runs[:-1:2]
+    return runs
